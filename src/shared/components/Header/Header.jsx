c15d505e818c65265cd2e5f672d1ca4a42ba3342
lexeika <alexseimoskalev750@gmail.com>
2025-09-27 16:52:07 +0300

    feat: added main layout for pages with header, right sidebar and footer sections

---
+++ src/shared/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from './Header.module.css';
+import searchIcon from './assets/search-icon.png';
+import cartIcon from './assets/cart-icon.png';
+import headerAvatar from './assets/header-avatar.png';
+
+function Header({ onSearch }) {
+  const [inputValue, setInputValue] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearchClick = () => {
+    onSearch(inputValue.trim());
+  }
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.left}>
+        <span className={styles.logo}>React</span>
+        <a href="#orders" className={styles.orders}>История заказов</a>
+      </div>
+
+      <div className={styles.center}>
+        <div className={styles.searchWrapper}>
+          <input 
+          type="text"
+          className={styles.search}
+          placeholder="Поиск бренда, товара, категории..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div
+            className={styles.iconWrapper}
+            onClick={handleSearchClick}
+          >
+            <img src={searchIcon} alt="Search Icon" />
+          </div>
+        </div>
+      </div>
+      
+      <div className={styles.right}>
+        <div 
+          className={styles.cartIconCircle}
+          onClick={() => navigate(`/cart`)}
+        >
+          <img src={cartIcon} alt="Корзина" />
+          <span className={styles.badge}></span>  
+        </div>
+        <img src={headerAvatar} alt="Профиль" className={styles.headerAvatar} />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
