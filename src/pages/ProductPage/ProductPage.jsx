36787b27ef96887b1f920b84e0db3d970abc438e
lexeika <alexseimoskalev750@gmail.com>
2025-09-27 17:18:20 +0300

    feat: added product details section for product page

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,7 +1,34 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState} from 'react';
+import MainLayout from '../../shared/layouts/MainLayout/MainLayout';
+import ProductDetails from '../../shared/components/ProductDetails/ProductDetails';
+import productsData from '../../api/products.json';
+import categoriesData from '../../api/categories.json';
+
 function ProductPage() {
-    return (
-        <></>
-    );
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [category, setCategory] = useState(null);
+
+  useEffect(() => {
+    const foundProduct = productsData.find(p => String(p.id) === id);
+    setProduct(foundProduct);
+
+    if(foundProduct) {
+      const foundCategory = categoriesData.find(cat => cat.id === foundProduct.categoryId);
+      setCategory(foundCategory);
+    }
+  }, [id])
+
+  return (
+    <MainLayout>
+      {product ? (
+        <ProductDetails product={product} category={category} />
+      ) : (
+        <div>Товар не найден</div>
+      )}
+    </MainLayout>
+  );
 }
 
 export default ProductPage;
